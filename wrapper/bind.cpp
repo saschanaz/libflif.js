3625283d624fd150d8f631e3b47605bf668ccca4
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-08-28 17:48:48 +0900

    compilation success

--- wrapper/bind.cpp
+++ wrapper/bind.cpp
@@ -3,43 +3,197 @@
 #include <emscripten/bind.h>
 using namespace emscripten;
 
-#include <flif-interface-private.hpp>
+#include <flif.h>
+#include <stdexcept>
 
-void setFlifCallback(FLIF_DECODER* decoder, size_t callback) {
-    decoder->callback = (void*)callback;
-}
+class FLIFImageWrapper {
+private:
+    FLIF_IMAGE* image;
+public:
+    FLIFImageWrapper(FLIF_IMAGE* image) {
+        this->image = image;
+    }
+    
+    static FLIFImageWrapper* create(uint32_t width, uint32_t height) {
+        return new FLIFImageWrapper(flif_create_image(width, height));
+    }
 
-void clearFlifCallback(FLIF_DECODER* decoder) {
-    decoder->callback = NULL;
-}
+    static FLIFImageWrapper* createHDR(uint32_t width, uint32_t height) {
+        return new FLIFImageWrapper(flif_create_image_HDR(width, height));
+    }
+    
+    ~FLIFImageWrapper() {
+        flif_destroy_image(this->image);
+    }
+
+    uint32_t width() const {
+        return flif_image_get_width(this->image);
+    }
+
+    uint32_t height() const {
+        return flif_image_get_height(this->image);
+    }
+
+    uint8_t nbChannels() const {
+        return flif_image_get_nb_channels(this->image);
+    }
+
+    uint8_t depth() const {
+        return flif_image_get_depth(this->image);
+    }
+
+    uint32_t frameDelay() const {
+        return flif_image_get_frame_delay(this->image);
+    }
+
+    void setFrameDelay(uint32_t delay) {
+        flif_image_set_frame_delay(this->image, delay);
+    }
+
+    void writeRowRGBA8(uint32_t row, size_t buffer, size_t bufferSizeBytes) {
+        flif_image_write_row_RGBA8(this->image, row, (void*)buffer, bufferSizeBytes);
+    }
+
+    void readRowRGBA8(uint32_t row, size_t buffer, size_t bufferSizeBytes) {
+        flif_image_read_row_RGBA8(this->image, row, (void*)buffer, bufferSizeBytes);
+    }
+
+    void writeRowRGBA16(uint32_t row, size_t buffer, size_t bufferSizeBytes) {
+        flif_image_write_row_RGBA16(this->image, row, (void*)buffer, bufferSizeBytes);
+    }
+
+    void readRowRGBA16(uint32_t row, size_t buffer, size_t bufferSizeBytes) {
+        flif_image_read_row_RGBA16(this->image, row, (void*)buffer, bufferSizeBytes);
+    }
+};
+
+class FLIFDecoderWrapper {
+private:
+    FLIF_DECODER* decoder;
+public:
+    FLIFDecoderWrapper() {
+        this->decoder = flif_create_decoder();
+    }
+
+    ~FLIFDecoderWrapper() {
+        flif_destroy_decoder(this->decoder);
+    }
+
+    void decodeFile(const std::string& filename) {
+        const int32_t result = flif_decoder_decode_file(this->decoder, filename.c_str());
+        if (result == 0) {
+            throw std::runtime_error("decodeFile failed");
+        }
+    }
+
+    void decodeMemory(size_t buffer, size_t buffer_size_bytes) {
+        const int32_t result = flif_decoder_decode_memory(this->decoder, (void*)buffer, buffer_size_bytes);
+        if (result == 0) {
+            throw std::runtime_error("decodeFile failed");
+        }
+    }
+
+    size_t numImages() const {
+        return flif_decoder_num_images(this->decoder);
+    }
+
+    size_t numLoops() const {
+        return flif_decoder_num_loops(this->decoder);
+    }
+
+    FLIFImageWrapper* getImage(size_t index) {
+        return new FLIFImageWrapper(flif_decoder_get_image(this->decoder, index));
+    }
+
+    bool abort() {
+        return flif_abort_decoder(this->decoder);
+    }
+
+    void setCRCCheck(bool crcCheck) {
+        flif_decoder_set_crc_check(this->decoder, crcCheck);
+    }
+
+    void setQuality(int32_t quality) {
+        flif_decoder_set_quality(this->decoder, quality);
+    }
+
+    void setScale(uint32_t scale) {
+        flif_decoder_set_scale(this->decoder, scale);
+    }
+
+    void setResize(uint32_t width, uint32_t height) {
+        flif_decoder_set_resize(this->decoder, width, height);
+    }
+
+    void setFit(uint32_t width, uint32_t height) {
+        flif_decoder_set_fit(this->decoder, width, height);
+    }
+
+    void setCallback(size_t callback) {
+        flif_decoder_set_callback(this->decoder, reinterpret_cast<uint32_t (*)(int32_t,int64_t)>(callback));
+    }
+
+    void setFirstCallbackQuality(int32_t quality) {
+        flif_decoder_set_first_callback_quality(this->decoder, quality);
+    }
+};
 
 EMSCRIPTEN_BINDINGS(libflifem) {
     // function("main", &main, allow_raw_pointers());
 
-    // class_<FlifDecoderWrapper>("FLIFDecoder")
-    //     .constructor()
-    //     ;
-
-    class_<FLIF_DECODER>("FLIFDecoder")
-        .constructor()
-        .function("decodeFile", &FLIF_DECODER::decode_file, allow_raw_pointers())
-        .function("decodeMemory", &FLIF_DECODER::decode_memory, allow_raw_pointers())
-        .function("abort", &FLIF_DECODER::abort)
-        .function("numImages", &FLIF_DECODER::num_images)
-        .function("numLoops", &FLIF_DECODER::num_loops)
-        .function("getImage", &FLIF_DECODER::get_image, allow_raw_pointers())
-        .property("quality", &FLIF_DECODER::quality)
-        .property("scale", &FLIF_DECODER::scale)
-        // .property("callback", &FLIF_DECODER::callback, allow_raw_pointers()) // Emscripten #4523
-        .property("firstQuality", &FLIF_DECODER::first_quality)
-        .property("rw", &FLIF_DECODER::rw)
-        .property("rh", &FLIF_DECODER::rh)
-        .property("crc_check", &FLIF_DECODER::crc_check)
-        .property("fit", &FLIF_DECODER::fit)
+    class_<FLIFImageWrapper>("FLIFImage")
+        .class_function("create", &FLIFImageWrapper::create, allow_raw_pointers())
+        .class_function("createHDR", &FLIFImageWrapper::createHDR, allow_raw_pointers())
+        .property("width", &FLIFImageWrapper::width)
+        .property("height", &FLIFImageWrapper::height)
+        .property("nbChannels", &FLIFImageWrapper::nbChannels)
+        .property("depth", &FLIFImageWrapper::depth)
+        .property("frameDelay", &FLIFImageWrapper::frameDelay, &FLIFImageWrapper::setFrameDelay)
+        .function("writeRowRGBA8", &FLIFImageWrapper::writeRowRGBA8)
+        .function("readRowRGBA8", &FLIFImageWrapper::readRowRGBA8)
+        .function("writeRowRGBA16", &FLIFImageWrapper::writeRowRGBA16)
+        .function("readRowRGBA16", &FLIFImageWrapper::readRowRGBA16)
         ;
+
+    class_<FLIFDecoderWrapper>("FLIFDecoder")
+        .constructor()
+        .function("decodeFile", &FLIFDecoderWrapper::decodeFile)
+        .function("decodeMemory", &FLIFDecoderWrapper::decodeMemory)
+        .property("numImages", &FLIFDecoderWrapper::numImages)
+        .property("numLoops", &FLIFDecoderWrapper::numLoops)
+        .function("getImage", &FLIFDecoderWrapper::getImage, allow_raw_pointers())
+        .function("abort", &FLIFDecoderWrapper::abort)
+        .function("setCRCCheck", &FLIFDecoderWrapper::setCRCCheck)
+        .function("setQuality", &FLIFDecoderWrapper::setQuality)
+        .function("setScale", &FLIFDecoderWrapper::setScale)
+        .function("setResize", &FLIFDecoderWrapper::setResize)
+        .function("setFit", &FLIFDecoderWrapper::setFit)
+        .function("setCallback", &FLIFDecoderWrapper::setCallback)
+        .function("setFirstCallbackQuality", &FLIFDecoderWrapper::setFirstCallbackQuality)
+        ;
+
     
-    function("setFLIFCallback", &setFlifCallback, allow_raw_pointers());
-    function("clearFLIFCallback", &clearFlifCallback, allow_raw_pointers());
+
+    // class_<FLIF_DECODER>("FLIFDecoder")
+    //     .constructor()
+    //     .function("decodeFile", &FLIF_DECODER::decode_file, allow_raw_pointers())
+    //     .function("decodeMemory", &FLIF_DECODER::decode_memory, allow_raw_pointers())
+    //     .function("abort", &FLIF_DECODER::abort)
+    //     .function("numImages", &FLIF_DECODER::num_images)
+    //     .function("numLoops", &FLIF_DECODER::num_loops)
+    //     .function("getImage", &FLIF_DECODER::get_image, allow_raw_pointers())
+    //     .property("quality", &FLIF_DECODER::quality)
+    //     .property("scale", &FLIF_DECODER::scale)
+    //     // .property("callback", &FLIF_DECODER::callback, allow_raw_pointers()) // Emscripten #4523
+    //     .property("firstQuality", &FLIF_DECODER::first_quality)
+    //     .property("rw", &FLIF_DECODER::rw)
+    //     .property("rh", &FLIF_DECODER::rh)
+    //     .property("crc_check", &FLIF_DECODER::crc_check)
+    //     .property("fit", &FLIF_DECODER::fit)
+    //     ;
+    
+    // function("setFLIFCallback", &setFlifCallback, allow_raw_pointers());
+    // function("clearFLIFCallback", &clearFlifCallback, allow_raw_pointers());
 }
 
 #endif
