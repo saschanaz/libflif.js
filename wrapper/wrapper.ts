d8a3053ade729c3e8ee6aca28aab2b1ed096c5f1
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-08-30 13:32:47 +0900

    AnimationDirector

--- wrapper/wrapper.ts
+++ wrapper/wrapper.ts
@@ -94,7 +94,7 @@ namespace libflif {
             debugLog(`sending data for ${type} to worker.`);
             if (bag.imageInfo) {
                 worker.postMessage({ type, uuid, input: arrayBuffer, imageInfo: bag.imageInfo })
-            } 
+            }
             else {
                 worker.postMessage({ type, uuid, input: arrayBuffer })
             }
@@ -117,5 +117,121 @@ namespace libflif {
 
     export function observeDOM() {
         throw new Error("not implemented yet")
+    }
+}
+
+
+interface AnimatedFrame {
+    data: Blob[];
+    width: number;
+    height: number;
+    frameDelay: number;
+}
+interface Animation {
+    frames: AnimatedFrame[];
+    loop: number;
+}
+// TODO: fork this to a separate repo
+class AnimationDirector {
+    private _animationFrames: AnimatedFrame[];
+    private _animationLoop: number;
+    private _nextFrameTime = 0;
+    private _currentFrameIndex = -1;
+    private _working = false;
+
+    get frames() {
+        return this._animationFrames.slice();
+    }
+
+    get currentFrameIndex() {
+        return this._currentFrameIndex;
+    }
+
+    get working() {
+        return this._working;
+    }
+
+    get duration() {
+        let duration = 0;
+        for (let frame of this._animationFrames) {
+            duration += frame.frameDelay;
+        }
+        return duration;
+    }
+
+    constructor(animation: Animation) {
+        if (!Array.isArray(this._animationFrames)) {
+            throw new Error("Invalid non-array frame array.")
+        }
+        this._animationFrames = this._cloneSanitizedFrames(animation.frames);
+        this._animationLoop = animation.loop || 0;
+
+        if (this._animationFrames.length <= 1) {
+            throw new Error(`Expected multiple frames but got ${this._animationFrames.length} frame.`);
+        }
+        if (isNaN(this._animationLoop) || this._animationLoop <= 0) {
+            throw new Error(`Invalid non-positive loop value.`)
+        }
+    }
+
+    async start(animate: (frame: AnimatedFrame) => any) {
+        if (this._working) {
+            return;
+        }
+        this._working = true;
+
+        let time = performance.now();
+        while (this._working) {
+            if (this._nextFrameTime <= time) {
+                this._currentFrameIndex++;
+                const currentFrame = this._animationFrames[this._currentFrameIndex];
+                this._nextFrameTime += currentFrame.frameDelay;
+                try {
+                    animate(currentFrame);
+                }
+                catch (err) {
+                    console.error(err);
+                }
+            }
+
+            time = await new Promise<number>(resolve => requestAnimationFrame(time => resolve(time)));
+        }
+    }
+
+    stop() {
+        if (!this._working) {
+            return;
+        }
+        this._working = false;
+    }
+
+    private _cloneSanitizedFrames(animatedFrames: AnimatedFrame[]) {
+        const result: AnimatedFrame[] = [];
+
+        for (let frame of animatedFrames) { // cannot use const because of Firefox
+            const sanitized = Object.freeze(Object.assign({}, frame));
+            sanitized.frameDelay = sanitized.frameDelay || 100;
+            sanitized.width = sanitized.width | 0;
+            sanitized.height = sanitized.height | 0;
+            if (!Array.isArray(sanitized.data)) {
+                throw new Error("Frame does not contain Blob data array.");
+            }
+            for (let blob of sanitized.data) {
+                if (blob.size === 0) {
+                    throw new Error("Frame contains zero sized blob data.");
+                }
+            }
+            if (sanitized.frameDelay <= 0) {
+                throw new Error("Frame delay must be positive value.");
+            }
+            if (sanitized.width <= 0) {
+                throw new Error("Frame width must be positive value.");
+            }
+            if (sanitized.height <= 0) {
+                throw new Error("Frame height must be positive value.");
+            }
+        }
+
+        return result;
     }
 }
