3c8ab0be719ba16d484eb91ee3c204e2eb14d09a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-08-31 15:06:02 +0900

    update lib

---
+++ lib/wrapper.d.ts
@@ -0,0 +1,37 @@
+declare namespace libflif {
+    var libDir: string;
+    var debug: boolean;
+}
+declare namespace libflif {
+    function startWorker(): void;
+    function decode(input: ArrayBuffer | Blob, callback: (result: libflifProgressiveDecodingResult) => any): Promise<void>;
+    function encode(input: libflifEncoderInput): Promise<ArrayBuffer>;
+    function observeDOM(): void;
+}
+interface AnimatedFrame {
+    data: Blob;
+    width: number;
+    height: number;
+    frameDelay: number;
+}
+interface Animation {
+    frames: AnimatedFrame[];
+    loop: number;
+}
+declare class AnimationDirector {
+    private _animationFrames;
+    private _animationLoop;
+    private _currentLoop;
+    private _currentFrameIndex;
+    private _working;
+    readonly frames: AnimatedFrame[];
+    readonly currentFrameIndex: number;
+    readonly working: boolean;
+    readonly duration: number;
+    constructor(animation: Animation);
+    alterFrames(frames: AnimatedFrame[]): void;
+    start(animate: (frame: AnimatedFrame) => any): Promise<void>;
+    private _safeAnimate(frame, animate);
+    stop(): void;
+    private _cloneSanitizedFrames(animatedFrames);
+}
